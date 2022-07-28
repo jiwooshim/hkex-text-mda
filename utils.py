0a2372bbf38ea2794188fcf8fc6ef4c1b4782108
jiwooshim <60967141+jiwooshim@users.noreply.github.com>
2022-07-28 17:19:47 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -142,44 +142,6 @@ def init_db_mda_text():
     cur.close()
 
 
-class PdfOutline(PyPDF2.PdfFileReader):
-    def getDestinationPageNumbers(self):
-        def _setup_outline_page_ids(outline, _result=None):
-            if _result is None:
-                _result = {}
-            for obj in outline:
-                if isinstance(obj, PyPDF2.pdf.Destination):
-                    _result[(id(obj), obj.title)] = obj.page.idnum
-                elif isinstance(obj, list):
-                    _setup_outline_page_ids(obj, _result)
-            return _result
-
-        def _setup_page_id_to_num(pages=None, _result=None, _num_pages=None):
-            if _result is None:
-                _result = {}
-            if pages is None:
-                _num_pages = []
-                pages = self.trailer["/Root"].getObject()["/Pages"].getObject()
-            t = pages["/Type"]
-            if t == "/Pages":
-                for page in pages["/Kids"]:
-                    _result[page.idnum] = len(_num_pages)
-                    _setup_page_id_to_num(page.getObject(), _result, _num_pages)
-            elif t == "/Page":
-                _num_pages.append(1)
-            return _result
-
-        outline_page_ids = _setup_outline_page_ids(self.getOutlines())
-        page_id_to_page_numbers = _setup_page_id_to_num()
-
-        result = {}
-        for (_, title), page_idnum in outline_page_ids.items():
-            if isinstance(title, PyPDF2.generic.TextStringObject):
-                title = title.encode('utf-8')
-            result[title] = page_id_to_page_numbers.get(page_idnum, '???')
-        return result
-
-
 def get_filenames(input_folder):
     sublist = os.listdir(input_folder)
     sublist.sort()
